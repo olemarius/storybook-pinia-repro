3f418807ab39437fc5f151a6b3f91e983769a441
Ole Marius Løset <olemariusloset@gmail.com>
2024-04-09 23:06:35 +0200

    fix: make debugging easier

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -1,5 +1,5 @@
 import type { StorybookConfig } from "@storybook/vue3-vite";
-
+import { mergeConfig } from 'vite';
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
@@ -11,6 +11,14 @@ const config: StorybookConfig = {
   framework: {
     name: "@storybook/vue3-vite",
     options: {},
+  },
+  async viteFinal(config, { configType }) {
+    return mergeConfig(config, {
+      build:{
+        minify: false,
+        sourcemap: true,
+      },
+    });
   },
   docs: {
     autodocs: "tag",
